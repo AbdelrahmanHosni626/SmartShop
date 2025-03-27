9dcc72d1222af313408603d5571a7303169e4da7
Abdelrahman Hosni <abdohosny626@gmail.com>
2025-03-27 06:16:09 +0200

    Initialize Firebase for the App

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:smartshop/bloc_observer.dart';
 import 'package:smartshop/core/routing/app_router.dart';
 import 'package:smartshop/smart_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
   Bloc.observer = MyBlocObserver();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     SmartApp(
       appRouter: AppRouter(),
