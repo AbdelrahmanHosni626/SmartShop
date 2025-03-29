5538437af66260b27dfac1c998cc040041b59179
Abdelrahman Hosni <abdohosny626@gmail.com>
2025-03-29 19:58:04 +0200

    Save Firebase Data after Signup.

--- lib/features/register/logic/register_cubit.dart
+++ lib/features/register/logic/register_cubit.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:smartshop/features/register/data/models/user_model.dart';
 import 'package:smartshop/features/register/logic/register_states.dart';
 
 class RegisterCubit extends Cubit<RegisterStates> {
@@ -51,10 +53,34 @@ class RegisterCubit extends Cubit<RegisterStates> {
         email: email,
         password: password,
       );
+      saveUserData(userEmail: email, userName: userName);
       print("email is: $email password is: $password **********************");
       emit(RegisterSuccessState());
     } on FirebaseAuthException catch (e) {
       emit(RegisterErrorState(e.toString()));
     }
+  }
+
+  Future<void> saveUserData({
+    required String userName,
+    required String userEmail,
+  }) async {
+    User user = credential.user!;
+    emit(SaveUserDataLoadingState());
+
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(user.uid)
+        .set({
+          "userId": user.uid,
+          "userName": userName,
+          "userEmail": userEmail,
+          "userImage": user.photoURL,
+          "createdAt": Timestamp.now(),
+          "userCart": [],
+          "userWishlist": [],
+        })
+        .then((value) => emit(SaveUserDataSuccessState()))
+        .catchError((error) => emit(SaveUserDataErrorState(error.toString())));
   }
 }
